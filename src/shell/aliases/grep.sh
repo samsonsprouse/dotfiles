ef7198c457538ee216328f5f69e1bfa67b51ccdd
Samson Sprouse <samson.sprouse@gmail.com>
2014-10-31 23:58:41 -0700

    Reorganized dotfiles

---
+++ src/shell/aliases/grep.sh
@@ -0,0 +1,4 @@
+alias grep='grep --color=auto'
+alias gi='grep -i'
+alias gv='grep -v'                    # Exclusive grep.
+alias gvi='grep -vi'                  # Exclusive grep sans case-sensitivity.
---
+++ src/shell/bash.sh
@@ -0,0 +1,54 @@
+
+# If not running interactively, don't do anything
+[ -z "$PS1" ] && return
+
+# don't put duplicate lines in the history. See bash(1) for more options
+# ... or force ignoredups and ignorespace
+HISTCONTROL=ignoredups:ignorespace
+
+# append to the history file, don't overwrite it
+shopt -s histappend
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+HISTSIZE=1000
+HISTFILESIZE=2000
+
+# check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
+
+
+#print your terminal's ANSI colors for codes 0-256
+print_colors () {
+    ( x=`tput op` y=`printf %$((${COLUMNS}-6))s`;for i in {0..256};do o=00$i;echo -e ${o:${#o}-3:3} `tput setaf $i;tput setab $i`${y// /=}$x;done; )
+}
+
+prompt_command () {
+    BLACK=$(tput setaf 0)
+    WHITE=$(tput setaf 15)
+    OFFWHITE=$(tput setaf 255)
+    NORMAL=$(tput sgr0)
+    GREEN=$(tput setaf 2)
+
+    if [ $? -eq 0 ]; then # set an error string for the prompt, if applicable
+        ERRPROMPT=" "
+    else
+        ERRPROMPT='-&gt;($?) '
+    fi
+    
+    if [ `date +%p` = "PM" ]; then
+        meridiem="pm"
+    else
+        meridiem="am"
+    fi
+
+    local gitbranch=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\* \1 /'`
+
+    local TIME=`date +"%l:%M:%S$meridiem"|sed 's/ //g'`
+    local myprompt="\[${OFFWHITE}\]_________________________________________________________\n"
+    myprompt=$myprompt"[\u@\h ${TIME}]$ERRPROMPT \w\n"
+    myprompt=$myprompt"\[${GREEN}\]${gitbranch}\[${OFFWHITE}\]> \[${NORMAL}\]"
+
+    export PS1=$myprompt
+}
+PROMPT_COMMAND=prompt_command
