2ff3414c52172e4828b5a83addd4bb0d003b917f
Samson <samson.sprouse@gmail.com>
2014-12-28 16:54:30 -0800

    More work to get rbenv working with fresh

--- src/shell/path/rvm.sh
+++ src/shell/path/rvm.sh
@@ -1,4 +1,4 @@
 # This loads RVM into a shell. 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
-PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+export PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
