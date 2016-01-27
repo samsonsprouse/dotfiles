5997c4dc6bec7a1792d33eeea34c24a360f53837
Samson Sprouse <samson.sprouse@gmail.com>
2016-01-26 16:55:05 -0800

    Update source_dir method

--- shell/functions/source_dir.sh
+++ shell/functions/source_dir.sh
@@ -1,7 +1,4 @@
 # wont work for files that contain spaces
 function source_dir {
-    find $1 -type f -name "*.sh" | while read f; do
-      echo "sourcing: $f"
-      source "$f"
-    done;
+  for f in $(find $1 -type f -name "*.sh"); do source "$f"; done
 }
